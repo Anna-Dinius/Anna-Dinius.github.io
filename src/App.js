94af68ae6299ea8a9949c8c21d84f90acd79119c
Anna-Dinius <ae.dinius03@gmail.com>
2025-04-02 22:34:18 -0400

    removed branch origin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Km2Mile from './Km2Mile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Km2Mile />
       </header>
     </div>
   );
